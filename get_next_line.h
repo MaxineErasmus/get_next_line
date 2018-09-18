9a5e486086b0528871dc25124a34d857c18ab92b
Rinus ERASMUS <rerasmus@e5r7p5.wethinkcode.co.za>
2018-09-18 11:08:07 +0200

    stable - final version

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rerasmus <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/11 10:12:50 by rerasmus          #+#    #+#             */
+/*   Updated: 2017/11/14 11:28:06 by rerasmus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 32
+
+# include <fcntl.h>
+# include "libft/libft.h"
+
+int		get_next_line(int const fd, char **line);
+
+#endif
